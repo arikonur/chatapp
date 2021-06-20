b05314deca201dfff9df97d8001518736d91f4be
onur <onurark12@gmail.com>
2021-06-20 20:43:08 +0300

    Created sms control required to login to the application and firebase connection done.

--- app/src/main/java/com/example/chatapp/PhoneNumberActivity.java
+++ app/src/main/java/com/example/chatapp/PhoneNumberActivity.java
@@ -2,13 +2,33 @@ package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.chatapp.databinding.ActivityPhoneNumberBinding;
 
 public class PhoneNumberActivity extends AppCompatActivity {
+
+    ActivityPhoneNumberBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_number);
+        binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        binding.phoneBox.requestFocus();
+
+        binding.continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(PhoneNumberActivity.this,OTPActivity.class);
+                intent.putExtra("phoneNumber",binding.phoneBox.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
