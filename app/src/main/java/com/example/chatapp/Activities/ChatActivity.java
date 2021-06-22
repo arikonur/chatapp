3d0ee480c963feea03a1a7004b1b5fe4acff2222
Onur Arık <62481825+arikonur@users.noreply.github.com>
2021-06-22 20:40:31 +0300

    Send messages from Application

--- app/src/main/java/com/example/chatapp/Activities/ChatActivity.java
+++ app/src/main/java/com/example/chatapp/Activities/ChatActivity.java
@@ -3,14 +3,18 @@ package com.example.chatapp.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.chatapp.Adapters.MessagesAdapter;
 import com.example.chatapp.Models.Message;
 import com.example.chatapp.databinding.ActivityChatBinding;
 import com.example.chatapp.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -19,6 +23,8 @@ public class ChatActivity extends AppCompatActivity {
     ArrayList<Message> messages;
 
     String senderRoom,recieverRoom;
+
+    FirebaseDatabase database;
 
 
     @Override
@@ -35,7 +41,41 @@ public class ChatActivity extends AppCompatActivity {
         String senderUid = FirebaseAuth.getInstance().getUid();
 
         senderRoom = senderUid + receiverUid;
-        receiverUid = receiverUid + senderUid;
+        recieverRoom = receiverUid + senderUid;
+
+        database=FirebaseDatabase.getInstance();
+
+        binding.sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText =binding.messageBox.getText().toString();
+
+                Date date = new Date();
+                Message message= new Message(messageText,senderUid,date.getTime());
+                binding.messageBox.setText("");
+
+
+                database.getReference().child("chat")
+                        .child(senderRoom)
+                        .child("messages")
+                        .setValue(message).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        database.getReference().child("chat")
+                                .child(recieverRoom)
+                                .child("messages")
+                                .setValue(message).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+
+                            }
+                        });
+
+                    }
+                });
+
+            }
+        });
 
         getSupportActionBar().setTitle(name);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
